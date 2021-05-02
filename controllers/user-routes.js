9ecc7a5773ab559bad6dc4fc3afa5c92e6326f8d
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-05-02 18:49:09 -0500

    Set up login route

---
+++ controllers/user-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+
+//Create new user
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// For when User attempts to login
+router.post('/login', async (req, res) => {
+  try {
+    const userLogin = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    // This checks the users email
+    if (!userLogin) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    //This checks the users password
+    const validPassword = await userLogin.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: userLogin, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
