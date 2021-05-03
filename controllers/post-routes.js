47b58b385a3caea4db242d09e3b212650d7e1d6a
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-05-02 19:24:28 -0500

    Set up route to view all comments

--- controllers/post-routes.js
+++ controllers/post-routes.js
@@ -1,16 +1,37 @@
-const router = require('express').Router();
-const { Post } = require('../models');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 
-router.post('/', async (req, res) => {
-    try {
-        const newPost = await Post.create({
-            title: req.body.title,
-            content: req.body.content
-        })
-        res.status(200).JSON(newPost);
+// To create a new post
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    res.status(200).JSON(newPost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-})
+// To get all comments
+router.get("/", async (req, res) => {
+  Post.findAll({
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["body"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  });
+});
